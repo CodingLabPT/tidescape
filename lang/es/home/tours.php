fd8f6d13b7671c7530aee852b700c40a72f0eb3a
mario <mariomendesdeveloper@gmail.com>
2024-12-12 16:37:26 +0000

    updates

--- lang/es/home/tours.php
+++ lang/es/home/tours.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title' => 'Tours cercanos',
+    'title' => 'Cruceros y tours en barco',
     'since' => 'Desde',
     'book_now' => 'Reservar Ahora',
     'no_tours' => '¡Sin tours disponibles por el momento!',
